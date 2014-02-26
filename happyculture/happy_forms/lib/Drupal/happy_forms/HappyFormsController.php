99f875e8ff8bffc5fd9b55c0a0600bd9259e19fd
Nicolas Meyer <nicolas@happyculture.coop>
2014-02-26 14:03:43 +0100

    Files used to demostrate routing and basic form API.
    Change-Id: Ie5ff8e1bd32a068ecc6372c10aacd276f0c75870

---
+++ happyculture/happy_forms/lib/Drupal/happy_forms/HappyFormsController.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\happy_forms\HappyFormsController.
+ */
+
+namespace Drupal\happy_forms;
+
+/**
+ * Controller for Happy Forms.
+ */
+class HappyFormsController {
+
+  /**
+   * Render a list of entries in the database.
+   */
+  public function Index() {
+    $output = 'Hello Happy !';
+
+    return $output;
+  }
+
+
+}
