dd2f68bfb4e0aa762d2b0ced46834b9347f7346d
Besfr <guillaume@happyculture.coop>
2015-02-03 16:35:15 +0100

    Add block module.
    Change-Id: Iaf5ebcbed1891830615a875d064ed5f5eed78b22

---
+++ happyculture/happy_block/src/Plugin/Block/LibraryBlock.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\user\Plugin\Block\LibraryBlock.
+ */
+
+namespace Drupal\happy_block\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Url;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Access\AccessResultAllowed;
+
+/**
+ * Provides a 'Call to visit' block.
+ *
+ * @Block(
+ *   id = "visit_library",
+ *   admin_label = @Translation("Visit library"),
+ * )
+ */
+class LibraryBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    // Keep the code but show this part only on the second part of the training
+    // once we have the link view mode.
+    if (false) {
+      // Query against our entities.
+      $query = \Drupal::entityQuery('node')
+        ->condition('status', 1)
+        ->condition('changed', REQUEST_TIME, '<');
+
+      $book_number = 2;
+      if (!is_null($book_number) && is_numeric($book_number)) {
+        $query->range(1, $book_number);
+      }
+
+      $nids = $query->execute();
+
+      // Load the storage manager of our entity.
+      $storage = \Drupal::entityManager()->getStorage('node');
+      // Now we can load the entities.
+      $nodes = $storage->loadMultiple($nids);
+
+      return entity_view_multiple($nodes, 'link');
+    }
+    else {
+      return array(
+        '#markup' => \Drupal::l(t('Visit the library'), new Url('happy_query.query', array(), array(
+          'attributes' => array(
+            'title' => t('Visit the library.'),
+          ),
+        ))),
+      );
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function access(AccountInterface $account) {
+    return AccessResultAllowed::allowedIfHasPermission($account, 'access_happy_library');
+  }
+
+}
