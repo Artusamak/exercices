bbee789a1e4a8455df5ef8a8b47e6f71557a9903
Besfr <guillaume@happyculture.coop>
2015-02-13 18:42:50 +0100

    Create a new content entity type.
    Change-Id: Ia53173c59e758af5622802125bd5ab391f5667e5

---
+++ happyculture/happy_book/src/Entity/Form/authorEntitySettingsForm.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\happy_book\Entity\Form\authorEntitySettingsForm.
+ */
+
+namespace Drupal\happy_book\Entity\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class authorEntitySettingsForm.
+ * @package Drupal\happy_book\Form
+ * @ingroup happy_book
+ */
+class authorEntitySettingsForm extends FormBase
+ {
+
+
+
+
+  /**
+   * Returns a unique string identifying the form.
+   *
+   * @return string
+   *   The unique string identifying the form.
+   */
+  public function getFormId() {
+    return 'authorEntity_settings';
+  }
+
+  /**
+   * Form submission handler.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param array $form_state
+   *   An associative array containing the current state of the form.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Empty implementation of the abstract submit class.
+  }
+
+
+  /**
+   * Define the form used for AuthorEntity  settings.
+   * @return array
+   *   Form definition array.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param array $form_state
+   *   An associative array containing the current state of the form.
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['authorEntity_settings']['#markup'] = 'Settings form for AuthorEntity. Manage field settings here.';
+    return $form;
+  }
+}
