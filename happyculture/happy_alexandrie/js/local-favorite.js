d68e2898803a56df65384d7c828a34807d5ab4a0
Besfr <guillaume@happyculture.coop>
2015-09-30 12:13:00 +0200

    Simplify the theme function and remove duplicate code.
    Change-Id: I433b7c24e6022db42a617a4610138e5ea691fa05

--- happyculture/happy_alexandrie/js/local-favorite.js
+++ happyculture/happy_alexandrie/js/local-favorite.js
@@ -2,27 +2,29 @@
 
   "use strict";
 
-  Drupal.HC = Drupal.HC || {};
-  Drupal.HC.Training = Drupal.HC.Training || {};
+  Drupal.localfav = Drupal.localfav || {};
 
-  Drupal.theme.hcBlockContent = function (localFavsData) {
-    var block = document.createElement('div');
-    block.setAttribute('class', 'block');
-    block.setAttribute('id', 'block-favorite');
-    var title = document.createElement('h2');
-    title.innerHTML = 'Your favorite';
-    block.appendChild(title);
-    var content = document.createElement('div');
-    content.setAttribute('class', 'content');
-    block.appendChild(content);
-    var list = document.createElement('ul');
-    localFavsData.forEach(function (fav) {
-      var item = document.createElement('li');
-      item.innerHTML = fav;
-      list.appendChild(item);
-    });
-    content.appendChild(list);
-    return block;
+  Drupal.theme.hcBlockContent = function () {
+    return '<div class="block" id="block-favorite"><h2>Your favorite</h2><div class="content"></div></div>';
+  };
+
+  Drupal.localfav.generateFavList = function (localFavsData) {
+    // Get data from the localStorage if their are not provided.
+    if (typeof localFavsData === 'undefined') {
+      var localFavsSerial = localStorage.getItem('Drupal.localFavs');
+      if (localFavsSerial) {
+        localFavsData = JSON.parse(localFavsSerial);
+      }
+    }
+    if (localFavsData) {
+      var list = document.createElement('ul');
+      localFavsData.forEach(function (fav) {
+        var item = document.createElement('li');
+        item.innerHTML = fav;
+        list.appendChild(item);
+      });
+      $('#sidebar-first .region-sidebar-first #block-favorite .content').append(list);
+    }
   };
 
   /**
@@ -30,6 +32,11 @@
    */
   Drupal.behaviors.setLocalFavorite = {
     attach: function (context, settings) {
+      // Add the favorite block.
+      if (!document.getElementById('block-favorite')) {
+        $('#sidebar-first .region-sidebar-first').append(Drupal.theme('hcBlockContent'));
+        Drupal.localfav.generateFavList();
+      }
       // Add favorite link to the page (for now based on node title).
       var text = document.createTextNode('favorite');
       var link = document.createElement('a');
@@ -50,27 +57,9 @@
         }
         localFavsData.push(newFav);
         localStorage.setItem('Drupal.localFavs', JSON.stringify(localFavsData));
-        var block = Drupal.theme('hcBlockContent', localFavsData);
-        $('#sidebar-first .region-sidebar-first #block-favorite').remove();
-        $('#sidebar-first .region-sidebar-first').append(block);
+        $('#sidebar-first .region-sidebar-first #block-favorite').replaceWith(Drupal.theme('hcBlockContent'));
+        Drupal.localfav.generateFavList(localFavsData);
       });
-    }
-  };
-
-  /**
-   * Create favorite block.
-   */
-  Drupal.behaviors.createBlockLocalFavorite = {
-    attach: function (context, settings) {
-      // Get data from the localStorage
-      var localFavsSerial = localStorage.getItem('Drupal.localFavs');
-      if (localFavsSerial) {
-        var localFavsData = JSON.parse(localFavsSerial);
-        if (!document.getElementById('block-favorite')) {
-          var block = Drupal.theme('hcBlockContent', localFavsData);
-          $('#sidebar-first .region-sidebar-first').append(block);
-        }
-      }
     }
   };
 
