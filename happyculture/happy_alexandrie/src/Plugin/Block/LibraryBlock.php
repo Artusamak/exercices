1ab194b21dad1072fd277e4310a59b2cbf9b3381
Besfr <guillaume@happyculture.coop>
2015-11-23 19:09:37 +0100

    Improve the code that create the block. Show link element usage.
    Change-Id: I1fb00239eb4e42208f47005e7206b67af413998e

--- happyculture/happy_alexandrie/src/Plugin/Block/LibraryBlock.php
+++ happyculture/happy_alexandrie/src/Plugin/Block/LibraryBlock.php
@@ -10,6 +10,7 @@ namespace Drupal\happy_alexandrie\Plugin\Block;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Url;
+use Drupal\Core\Link;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Access\AccessResultAllowed;
 
@@ -27,57 +28,22 @@ class LibraryBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
-    // Keep the code but show this part only on the second part of the training
-    // once we have the link view mode.
-    // @TODO:
-    // Find the right way to manage the two potential behaviors.
-    if (FALSE) {
-      // Query against our entities.
-      $query = \Drupal::entityQuery('node')
-        ->condition('status', 1)
-        ->condition('type', 'happy_book')
-        ->condition('changed', REQUEST_TIME, '<');
-
-      // @TODO:
-      // Document this portion. Find a way to have something more consistent?
-      // What are the intentions?
-      $book_number = 2;
-      if (!is_null($book_number) && is_numeric($book_number)) {
-        $query->range(1, $book_number);
-      }
-
-      $nids = $query->execute();
-
-      // Load the storage manager of our entity.
-      $storage = \Drupal::entityManager()->getStorage('node');
-      // Now we can load the entities.
-      $nodes = $storage->loadMultiple($nids);
-
-      // Make sure that we have content to display to the user, otherwise
-      // display a polite message.
-      if (count($nodes) > 0) {
-        return entity_view_multiple($nodes, 'link');
-      }
-      else {
-        return array(
-          '#markup' => "<p>Aucun noeud n'est disponible pour le moment !</p>",
-        );
-      }
-    }
-    else {
-      $options = array(
-        'attributes' => array(
-          'title' => $this->link_label(),
-        ),
-      );
-      return array(
-        '#type' => 'link',
-        '#title' => $this->link_label(),
-        '#url' => new Url('happy_alexandrie.query_welcome_controller', array(), $options),
-        '#prefix' => '<p>',
-        '#suffix' => '</p>',
-      );
-    }
+    $options = array(
+      'attributes' => array(
+        'title' => $this->link_label(),
+      ),
+    );
+    return array(
+      '#type' => 'link',
+      '#title' => $this->link_label(),
+      '#url' => new Url('happy_alexandrie.query_welcome_controller', array(), $options),
+      '#prefix' => '<p>',
+      '#suffix' => '</p>',
+    );
+    // Other ways to do it. Directly using the link element.
+    return [
+      Link::createFromRoute($this->configuration['link_title'], 'happy_alexandrie.query_welcome_controller')->toRenderable(),
+    ];
   }
 
   /**
@@ -87,32 +53,24 @@ class LibraryBlock extends BlockBase {
     return AccessResultAllowed::allowedIfHasPermission($account, 'access alexandrie library');
   }
 
-  /**
-   * Returns the custom label of the link.
-   *
-   * @return string
-   *   The link label.
+    /**
+   * {@inheritdoc}
    */
-  public function link_label() {
-    if (!empty($this->configuration['link_label'])) {
-      return $this->configuration['link_label'];
-    }
-    else {
-      return t('Visit the library.');
-    }
+  public function defaultConfiguration() {
+    return array(
+      'link_title' => $this->t('Visit the library'),
+    );
   }
 
   /**
    * {@inheritdoc}
    */
   public function blockForm($form, FormStateInterface $form_state) {
-    $form = parent::blockForm($form, $form_state);
-
     $form['link_label'] = array(
       '#type' => 'textfield',
       '#title' => $this->t('Label of the link'),
       '#maxlength' => 255,
-      '#default_value' => $this->link_label(),
+      '#default_value' => $this->configuration['link_title'],
     );
     return $form;
   }
@@ -121,7 +79,6 @@ class LibraryBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function blockSubmit($form, FormStateInterface $form_state) {
-    // Save our custom settings when the form is submitted.
     $this->setConfigurationValue('link_label', $form_state->getValue('link_label'));
   }
 }
